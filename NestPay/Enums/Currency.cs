99aaeffdf96c0ffb3e531f868d2ecb1ddb9bade6
Haitham Basim <h.b.h.irs@outlook.com>
2024-11-27 17:56:39 +0300

    add currency enum class

---
+++ NestPay/Enums/Currency.cs
@@ -0,0 +1,25 @@
+namespace NestPay.Enums
+{
+    public enum Currency
+    {
+        /// <summary>
+        /// Iraqi Dinar
+        /// </summary>
+        IQD = 368,
+
+        /// <summary>
+        /// Turkish Lira
+        /// </summary>
+        TRY = 949,
+
+        /// <summary>
+        /// United States Dollar
+        /// </summary>
+        USD = 840,
+
+        /// <summary>
+        /// Euro
+        /// </summary>
+        EUR = 978,
+    }
+}
